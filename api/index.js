45a85cb2c56994c1789e99bd94858216affd07d4
Siphelele <sphllzulu@gmail.com>
2024-12-26 19:36:35 +0200

    added a server that points to the json server

--- api/index.js
+++ api/index.js
@@ -1,43 +1,13 @@
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-// const cors= require('cors')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = 3000
 
-// server.use(middlewares)
-// server.use(router)
-// server.use(cors)
+server.use(middlewares)
+server.use(router)
+server.use(cors());
 
-
-
-// module.exports = server
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const path = require('path');
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-const middlewares = jsonServer.defaults({
-  readOnly: false,
-  noCors: false
-});
-
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(router);
-
-module.exports = (req, res) => {
-  return server(req, res);
-};
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
+})
